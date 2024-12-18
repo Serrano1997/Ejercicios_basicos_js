cc740e0f5668409d3f64c49476762a17b8a7b7cc
Serrano1997 <vicentserrano1997@gmail.com>
2024-12-18 14:29:16 +0100

    Commit: Ejercicio 1

---
+++ Ejercicio1.js
@@ -0,0 +1,29 @@
+/*Ejercicio 1
+1. Define tu superhéroe favorito:
+Crea una variable llamada miSuperheroeFavorito y asigna el valor "Hulk".
+2. Define un número:
+Crea una variable llamada numeroFavorito y asigna el valor 50.
+3. Define las dimensiones:
+Crea una variable llamada altura y asigna el valor 5.
+Crea una variable llamada longitud y asigna el valor 10.
+4. Calcula la suma:
+Crea una variable llamada suma y asigna el valor de altura + longitud.
+5. Imprime los resultados:
+Muestra todas las variables por consola. */
+
+
+// Definir Superhéroe
+let miSuperheroeFavorito ="Hulk";
+// Definir un número
+let numeroFavorito = 50;
+// definir las dimenciones
+let altura = 5;
+let longitud = 10;
+// Calcular la suma
+let suma = altura + longitud;
+
+console.log(miSuperheroeFavorito);
+console.log(numeroFavorito);
+console.log(altura);
+console.log(longitud);
+console.log(suma);
